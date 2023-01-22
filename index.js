a51602bc7eaf53c8b070a3f84999b4fd812f9f28
Marcos <marcosd.a.chaves2@gmail.com>
2023-01-22 08:51:30 -0300

    Ejercicio Hola Mundo

---
+++ index.js
@@ -0,0 +1,14 @@
+const fn = () => {
+    const miString = "Soy un string "
+    let b = miString + " Muy feliz"
+    console.log (b)
+    console.log('Hola mundo');
+    
+}
+fn()
+
+const Clickeando =() => {
+    let a = "chanchito feliz"
+
+    console.log(a)
+}
